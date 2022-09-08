3ebe60224d5917689bac7727900696ef863007b9
Jadis <49281043+jadis0x@users.noreply.github.com>
2022-09-08 23:35:52 +0300

    Update MelonMain.cs

--- DevourClient/MelonMain.cs
+++ DevourClient/MelonMain.cs
@@ -255,6 +255,7 @@ namespace DevourClient
                 this.item_esp = GUI.Toggle(new Rect(Settings.Settings.x + 390, Settings.Settings.y + 280, 150, 20), this.item_esp, "Item ESP");
                 
                 Load.unlimitedUV = GUI.Toggle(new Rect(Settings.Settings.x + 200, Settings.Settings.y + 220, 150, 20), Load.unlimitedUV, "Unlimited UV");
+                Load._walkInLobby = GUI.Toggle(new Rect(Settings.Settings.x + 200, Settings.Settings.y + 250, 150, 20), Load._walkInLobby, "Walk In Lobby");
 
                 if (GUI.Button(new Rect(Settings.Settings.x + 10, Settings.Settings.y + 40, 150, 20), "Unlock Achievements"))
                 {
