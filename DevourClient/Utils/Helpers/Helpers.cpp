761b86fc08b7773b00c3b02b3fce66f7fa70f458
jadis0x <49281043+jadis0x@users.noreply.github.com>
2022-10-01 22:08:18 +0300

    add: isPlayerCrawling function

--- DevourClient/Utils/Helpers/Helpers.cpp
+++ DevourClient/Utils/Helpers/Helpers.cpp
@@ -1,3 +1,6 @@
 #include "Helpers.hpp"
 
 //define functions the same as in misc.hpp/cpp
+bool Helpers::isPlayerCrawling() {
+    return Players::LocalPlayer->GetComponent("NolanBehaviour")->CallMethod<bool*>("IsCrawling");
+}
