5d56a1dcfc844b2f028c31313e991db280709ac7
jadis0x <49281043+jadis0x@users.noreply.github.com>
2022-10-01 22:38:40 +0300

    add: IsInGame function

--- DevourClient/Utils/Helpers/Helpers.cpp
+++ DevourClient/Utils/Helpers/Helpers.cpp
@@ -4,3 +4,18 @@
 bool Helpers::isPlayerCrawling() {
     return Players::LocalPlayer->GetComponent("NolanBehaviour")->CallMethod<bool*>("IsCrawling");
 }
+
+bool Helpers::IsInGame() {
+    Unity::CGameObject* OptionsHelpers = Unity::GameObject::Find("OptionsHelpers");
+    Unity::CComponent* OptionsHelpersData = OptionsHelpers->GetComponent("OptionsHelpers");
+
+    if (OptionsHelpers == NULL) {
+        return false;
+    }
+
+    if (OptionsHelpersData == NULL) {
+        return false;
+    }
+
+    return OptionsHelpersData->GetMemberValue<bool*>("inGame");
+}
