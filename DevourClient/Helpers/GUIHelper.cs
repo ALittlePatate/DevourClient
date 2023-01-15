26a9607853ecd465dd6f94faccaf7a9b093954c7
jadis0x <49281043+jadis0x@users.noreply.github.com>
2023-01-15 22:08:27 +0300

    MakeTex function added

--- DevourClient/Helpers/GUIHelper.cs
+++ DevourClient/Helpers/GUIHelper.cs
@@ -33,5 +33,18 @@ namespace DevourClient.Helpers
 
             return color;
         }
+        
+        private Texture2D MakeTex(int width, int height, Color col)
+        {
+            Color[] pix = new Color[width * height];
+            for (int i = 0; i < pix.Length; ++i)
+            {
+                pix[i] = col;
+            }
+            Texture2D result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
+        }
     }
 }
